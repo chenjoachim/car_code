baaa2596c732184da0f2c97358dd2410ee723873
Wesley_Chen <1011.5131001w@gmail.com>
2022-04-15 12:11:37 +0800

    Node dict. build and BFS_two_points sketch

--- Algorithm/maze.py
+++ Algorithm/maze.py
@@ -1,10 +1,11 @@
+from re import L
 from node import *
 import numpy as np
 import csv
 import pandas
 from enum import IntEnum
 import math
-
+import queue
 
 class Action(IntEnum):
     ADVANCE = 1
@@ -27,8 +28,8 @@ class Maze:
         # Add nodes to the list "self.nodes"
         self.nodes = []
 
-        direction = {'North':'ND', 'South':'SD', 'West':'WD', 'East':'ED'}
-        dir_enum = {'North': Direction.NORTH, 'South': Direction.SOUTH, 'West': Direction.WEST, 'East': Direction.EAST}
+        direction = {'North':['ND', Direction.NORTH], 'South':['SD', Direction.SOUTH],\
+                    'West':['WD', Direction.WEST], 'East':['ED', Direction.EAST]}
         
         for i in range(0, self.num):
             _index = self.raw_data['index'][i]   # obtain the index of a node
@@ -37,13 +38,18 @@ class Maze:
             for dir in direction.keys():           # dir = 'North', 'South', 'West', 'East'
                 _succ = self.raw_data[dir][i]      # obtain the successor of each direction
                 if not np.isnan(_succ):
-                    _dist = self.raw_data[direction[dir]][i]          # obtain the distance of each direction
-                    _node.setSuccessor(_succ, dir_enum[dir], _dist)   # set successor (dir_enum[dir] is a Direction object)
+                    _dist = self.raw_data[direction[dir][0]][i]           # obtain the distance of each direction
+                    _node.setSuccessor(_succ, direction[dir][1], _dist)   # set successor (dir_enum[dir] is a Direction object)
     
             self.nodes.append(_node)
-            
-        self.nd_dict = dict()  # key: index, value: the correspond node
+        
+        # A dictionary with the key be the index of the node and the value be the corresponding "Node" object
+        # For example, self.nd_dict[1] represents the node of index 1
+        self.nd_dict = dict()
+        for _node in self.nodes:
+            self.nd_dict[_node.getIndex()] = _node 
 
+    # return the node of index 1 (starting point)
     def getStartPoint(self):
         if (len(self.nd_dict) < 2):
             print("Error: the start point is not included.")
@@ -58,9 +64,33 @@ class Maze:
         # Tips : return a sequence of nodes from the node to the nearest unexplored deadend
         return None
 
-    def BFS_2(self, nd_from, nd_to):
+    def BFS_two_points(self, nd_from, nd_to):
         # TODO : similar to BFS but with fixed start point and end point
         # Tips : return a sequence of nodes of the shortest path
+
+        # define the constant (parameters needed to be tested and revised)
+        STRAIGHT = 1.0   # the time taken to go straight line per length unit
+        TURN = 0.0       # the time taken to turn left or turn right per time
+        REVRESE = 0.0    # the time taken to reverse once
+
+        visit = [False for i in range (0, self.num)]  # an array determine whether a point is visited
+        visit[nd_from] = True
+
+        # Begin to BFS, q_bfs means the queue for BFS
+        # q_bfs storing three-termed tuple, (current_point, the time has been taken, the direction of the last point to this point)
+        q_bfs = queue.Queue()      
+        q_bfs.put((nd_from, 0, 0))  
+
+        while True:
+            if q_bfs.empty(): 
+                print("QueueEmptyError")
+                break
+            
+            
+
+
+
+        
         return None
 
     def getAction(self, car_dir, nd_from, nd_to):
